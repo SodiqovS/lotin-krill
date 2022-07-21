b7e95d0c8c759415e1e89cd8ec495d42b60b117f
sodiqdev <sodiqov2002@inbox.ru>
2022-07-21 16:22:41 +0500

    lotin va krillga bog'lab qo'yildi

--- lotin.js
+++ lotin.js
@@ -1,16 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
 
-const Transliterator = lotinKirill.default;
-const transliterator = new Transliterator();
+    const Transliterator = lotinKirill.default;
+    const transliterator = new Transliterator();
+    
+    const text = document.querySelector('#text'),
+        trText = document.querySelector('#trText'),
+        lotin = document.querySelector('#lotin'),
+        krill = document.querySelector('#krill');
 
-const text = document.querySelector('#text'),
-    trText = document.querySelector('#trText');
+    lotin.addEventListener('click', () => {
+        text.addEventListener('input', () => {
+            let cyrillicText = transliterator.textToCyrillic(text.value);
+            trText.textContent = cyrillicText;
+        })
+    })
+    
+    krill.addEventListener('click', () => {
+        trText.addEventListener('input', () => {
+            let latinText = transliterator.textToLatin(trText.value);
+            text.textContent = latinText;
+        })
+    });
+    
+    
 
-text.addEventListener('input', () => {
-    let cyrillicText = transliterator.textToCyrillic(text.value);
-    trText.textContent = cyrillicText;
+
 })
 
-trText.addEventListener('input', () => {
-    let latinText = transliterator.textToLatin(trText.value);
-    text.textContent = latinText;
-})
